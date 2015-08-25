0a86618042af1a454020f40e0c73440713205f77
André Felipe Azevedo <andre.felipe.az@gmail.com>
2015-08-25 12:09:41 -0300

    Agrupando Rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,27 @@ Route::post('oauth/access_token', function(){
     return Response::json(Authorizer::issueAccessToken());
 });
 
+
+Route::group(['middleware'=>'oauth'], function(){
+
+    Route::resource('client', 'ClientController', [ 'except' => ['create', 'edit'] ]);
+
+    Route::group(['prefix'=>'project'], function(){
+        Route::resource('', 'ProjectController', [ 'except' => ['create', 'edit'] ]);
+
+
+        Route::get('{id}/note', 'ProjectNoteController@index');
+        Route::post('{id}/note/', 'ProjectNoteController@store');
+        Route::get('{id}/note/{noteId}', 'ProjectNoteController@show');
+        Route::put('{id}/note/{noteId}', 'ProjectNoteController@update');
+        Route::delete('{id}/note/{noteId}', 'ProjectNoteController@destroy');
+    });
+
+
+
+});
+
+
 Route::get('client', ['middleware'=>'oauth','uses'=>'ClientController@index']);
 Route::post('client', 'ClientController@store');
 Route::get('client/{id}', 'ClientController@show');
