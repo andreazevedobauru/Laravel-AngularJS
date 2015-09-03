2fd65462f5517ad870a128982d2bcafffa82fa77
André Felipe Azevedo <andre.felipe.az@gmail.com>
2015-09-03 10:52:53 -0300

    Tarefa Copy-html Criado gulp Task-html pra copiar os htmls

--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,16 @@ config.vendor_path_css = [
     config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css'
 ];
 
+config.build_path_html = config.build_path + '/views';
+
+gulp.task('copy-html', function(){
+    gulp.src([
+        config.assets_path + '/js/views/**/*.html'
+    ])
+        .pipe(gulp.dest(config.build_path_html))
+        .pipe(livereload());
+
+});
 
 gulp.task('copy-styles', function(){
     gulp.src([
@@ -67,6 +77,7 @@ gulp.task('clear-build-folder', function(){
 });
 
 gulp.task('default', ['clear-build-folder'], function(){
+   gulp.start('copy-html');
    elixir(function(mix){
        mix.styles(config.vendor_path_css.concat([config.assets_path+'/css/**/*.css']), 'public/css/all.css', config.assets_path);
        mix.scripts(config.vendor_path_js.concat([config.assets_path+'/js/**/*.js']), 'public/js/all.js', config.assets_path);
@@ -76,8 +87,8 @@ gulp.task('default', ['clear-build-folder'], function(){
 
 gulp.task('watch-dev', ['clear-build-folder'], function(){
     livereload.listen();
-    gulp.start('copy-styles', 'copy-scripts');
-    gulp.watch(config.assets_path+'/**', ['copy-styles', 'copy-scripts']);
+    gulp.start('copy-styles', 'copy-scripts', 'copy-html');
+    gulp.watch(config.assets_path+'/**', ['copy-styles', 'copy-scripts', 'copy-html']);
 });
 
 /*
