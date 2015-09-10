c9ffba63a1489349051569ebcea09c3541676a83
André Felipe Azevedo <andre.felipe.az@gmail.com>
2015-09-10 16:59:16 -0300

    Adicionado o transformPresenter no Get dos Clients.js

--- public/build/js/services/client.js
+++ public/build/js/services/client.js
@@ -6,6 +6,16 @@ angular.module('app.services')
         return $resource(appConfig.baseUrl + '/client/:id', {id: '@id'}, {
             update: {
                 method: 'PUT'
+            },
+            query: {
+                method: 'GET',
+                isArray: true,
+                transformResponse: function(data, headers){
+                    //transformando o retorno em json
+                    var dataJson = JSON.parse(data);
+                    dataJson = dataJson.data;
+                    return dataJson;
+                }
             }
         });
     }]);
