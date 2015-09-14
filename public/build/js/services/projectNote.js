11dd3224ca201964c177b7e2fb044362a1c64433
André Felipe Azevedo <andre.felipe.az@gmail.com>
2015-09-14 16:54:00 -0300

    Retirado transformResponse

--- public/build/js/services/projectNote.js
+++ public/build/js/services/projectNote.js
@@ -6,21 +6,6 @@ angular.module('app.services')
         return $resource(appConfig.baseUrl + '/project/:id/note/:idNote', {id: '@id', idNote: '@idNote'}, {
             update: {
                 method: 'PUT'
-            },
-            get: {
-                method: 'GET',
-                transformResponse: function (data, headers) {
-                    var headersGetter = headers();
-                    if (headersGetter['content-type'] == 'application/json' || headersGetter['content-type'] == 'text/json') {
-                        var dataJson = JSON.parse(data);
-                        //transformando o retorno em json
-                        if (dataJson.hasOwnProperty('data')) {
-                            dataJson = dataJson.data;
-                        }
-                        return dataJson[0];
-                    }
-                    return data;
-                }
             }
         });
     }]);
