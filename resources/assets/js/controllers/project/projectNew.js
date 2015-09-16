032970e749f525ba9a8f9c529f8d79c4918bc74b
André Felipe Azevedo <andre.felipe.az@gmail.com>
2015-09-16 11:16:53 -0300

    Select com clients adicionado

--- resources/assets/js/controllers/project/projectNew.js
+++ resources/assets/js/controllers/project/projectNew.js
@@ -2,14 +2,15 @@
  * Created by andre on 09/09/2015.
  */
 angular.module('app.controllers')
-    .controller('ProjectNewController', ['$scope', '$location', 'Project',
-        function ($scope, $location, Project) {
+    .controller('ProjectNewController', ['$scope', '$location', 'Project', 'Client',
+        function ($scope, $location, Project, Client) {
             $scope.project = new Project();
+            $scope.clients = new Client.query();
 
             $scope.save = function () {
                 if($scope.form.$valid){
                     $scope.project.$save().then(function () {
-                        $location.path('/projects);
+                        $location.path('/projects');
                     });
                 }
             }
