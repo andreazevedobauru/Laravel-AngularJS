3c058181be83455a48286da9cbdf09d6c6ce4251
Andre Macbook <andre.felipe.az@gmail.com>
2015-10-24 15:38:42 -0200

    Criando Serviço URL

--- public/build/js/controllers/project-file/projectFileNew.js
+++ public/build/js/controllers/project-file/projectFileNew.js
@@ -2,16 +2,24 @@
  * Created by andre on 09/09/2015.
  */
 angular.module('app.controllers')
-    .controller('ProjectFileNewController', ['$scope', '$location', '$routeParams',
-        function ($scope, $location, $routeParams) {
-            //$scope.projectFile = new ProjectFile();
-            //$scope.projectFile.project_id  = $routeParams.id;
+    .controller('ProjectFileNewController', ['$scope', '$location', '$routeParams', 'Upload',
+        function ($scope, $location, $routeParams, Upload) {
+            $scope.projectFile = {
+                project_id: $routeParams.id
+            };
 
             $scope.save = function () {
                 if($scope.form.$valid){
-                    $scope.projectFile.$save({id: $routeParams.id}).then(function () {
+                    Upload.upload({
+                        url: '',
+                        data: {file: $scope.projectFile.file,  'name': $scope.projectFile.name, 'description': $scope.projectFile.description }
+                    }).then(function (resp) {
                         $location.path('/project/'+$routeParams.id+'/files');
+                        //console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
                     });
                 }
+                /*$scope.projectFile.$save({id: $routeParams.id}).then(function () {
+                 $location.path('/project/'+$routeParams.id+'/files');
+                 });*/
             }
         }]);
